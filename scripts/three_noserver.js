369cd6b96c09b874ddff75e3290fd47d7e33df03
kubitschekberlin <stephan.kubitschek@outlook.com>
2024-04-19 19:06:47 +0200

    Added HorizonShere - does not work due to CORS

--- scripts/three_noserver.js
+++ scripts/three_noserver.js
@@ -2,8 +2,10 @@ import $ from 'jquery'
 import { Events3D } from './events_3d.js';
 import RenderScene from '../library/render_scene.js'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { RenderHorizonSphere } from '../library/render_object.js';
 
 $(function () {
   const scene = new RenderScene('#canvas_parent');
   new Events3D(scene.camera, scene.scene, scene.animate);
+  //new RenderHorizonSphere(scene.scene, './dist/dolomiten_panorama_1.jpg');
 });
