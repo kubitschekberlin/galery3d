69bde16019d349b82c54127f0c1f9a93ae23a4ed
kubitschekberlin <stephan.kubitschek@outlook.com>
2024-10-11 17:30:33 +0200

    introduced handlebars

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import { Handlebars } from 'handlebars'
+import { create } from'express-handlebars';
 import path from 'path';
 import { __dirname } from './files.js';
 
@@ -7,6 +9,11 @@ const port = 3001;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
+
+// Setze Handlebars als Template-Engine
+const hbs = create({handlebars: Handlebars});
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
