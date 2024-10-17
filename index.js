76d22f81b35b9324430a1d24eb192804701c4b8a
kubitschekberlin <stephan.kubitschek@outlook.com>
2024-10-17 12:33:29 +0200

    No error with handlebars templates

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import pkg from 'handlebars'
-const { Handlebars } = pkg;
 import { create } from'express-handlebars';
 import path from 'path';
 import { __dirname } from './files.js';
@@ -12,12 +11,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 
 // Setze Handlebars als Template-Engine
-const hbs = create({handlebars: Handlebars});
+const hbs = create({
+  handlebars: pkg,
+  partialsDir: __dirname + '/views/partials',
+  defaultLayout: false,
+  compilerOptions: {}
+});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+  //res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.render('main', {});
 });
 
 app.listen(port, () => {
