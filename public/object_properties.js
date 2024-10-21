e1474a1e7e3a093b53512bd8ae7517049c69cc70
kubitschekberlin <stephan.kubitschek@outlook.com>
2024-10-21 16:44:15 +0200

    Object display

--- public/object_properties.js
+++ public/object_properties.js
@@ -1,14 +1,17 @@
 import $ from 'jquery';
 import ejs from 'ejs';
 import propertiesTemplate from '../views/partials/_object-properties.ejs';
-import itemTemplate from '../views/partials/_print-object-item.ejs';
+import itemTemplate from '../views/partials/_render-item.ejs';
+import objectTemplate from '../views/partials/_render-object.ejs';
 
 export class ObjectProperties {
   show = (object) => {
     console.log(object);
+    const renderObject = (key, value) => { return ejs.render(objectTemplate, {key: key, value: value}) },
+      renderItem = (key, value) => { return ejs.render(itemTemplate, {key: key, value: value, renderObject: renderObject}) }
     const html = ejs.render(propertiesTemplate, { 
       data: object,
-      renderItem: (key, value) => { return ejs.render(itemTemplate, {key: key, value: value}) }
+      renderItem: renderItem
      });
     console.log(html);
     $('#object_properties').html(html);
