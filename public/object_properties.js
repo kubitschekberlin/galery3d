2cfc1981218b1d0a8a44cc7d0004a63b3b2b5fe1
kubitschekberlin <stephan.kubitschek@outlook.com>
2024-11-12 18:36:20 +0100

    Object popups

--- public/object_properties.js
+++ public/object_properties.js
@@ -1,18 +1,43 @@
 import $ from 'jquery';
 import ejs from 'ejs';
-import propertiesTemplate from '../views/partials/_object-properties.ejs';
-import itemTemplate from '../views/partials/_render-item.ejs';
-import objectTemplate from '../views/partials/_render-object.ejs';
+import itemTemplate from '../views/partials/_print-item.ejs';
+import objectTemplate from '../views/partials/_print-object.ejs';
 
 export class ObjectProperties {
-  show = (object) => {
+  #popoverNumber = 0;
+
+  show = (name, object) => {
     console.log(object);
-    const renderObject = (key, value) => { return ejs.render(objectTemplate, {key: key, value: value}) },
-      renderItem = (key, value) => { return ejs.render(itemTemplate, {key: key, value: value, renderObject: renderObject}) }
-    const html = ejs.render(propertiesTemplate, { 
-      data: object,
-      renderItem: renderItem
-     });
+
+    const popoverID = () => {
+      return 'popover_' + this.#popoverNumber;
+    };
+    
+    // printObject: Erzeugt popup mit den Daten eines Objekts
+    // printItem: Erzeugt Zeile mit key-value Paar. Ist value ein Objekt, wird es als Schaltfläche zum rekursiven Öffnen eines
+    //             weiteren Objekts dargestellt.
+    const printObject = (name, data, print) => { 
+      const html = ejs.print(objectTemplate, {
+        name: name, 
+        data: data, 
+        printItem: print,
+        popoverID: popoverID()
+      });
+      this.#popoverNumber += 1;
+      return html;
+    };
+
+    const printItem = (key, value, id) => { 
+      const html = ejs.print(itemTemplate, {
+        key: key, 
+        value: value, 
+        printObject: printObject,
+        popoverID: id
+      });
+      return html;
+    };
+
+    const html = printObject(name, object, printItem);
     console.log(html);
     $('#object_properties').html(html);
   }
